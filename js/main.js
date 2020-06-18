fbf5833f08afe492d105ac61ab90e7638a0debff
MFoxx <mgally@outlook.com>
2020-06-18 22:20:00 +0200

    Minor changes + js documentation

--- js/main.js
+++ js/main.js
@@ -12,15 +12,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => { //Vyberie vsetky h
 
 
 // Display more
-const cards = document.querySelectorAll('.myCard');
+const cards = document.querySelectorAll('.myCard'); // Getting all cards
 
+// Setting forEach of that cards
 cards.forEach(card => {
+    // Gettin all needen elements
     const dots = card.querySelector('.dots');
     const readMore = card.querySelector('.readMore');
     const more = card.querySelector('.more');
     const readLess = card.querySelector('.readLess')
 
-
+    // Eventlistner on readMore click, adding and removing classes with display: none;
     readMore.addEventListener('click', ()=> {
         readMore.classList.add('none');
         more.classList.remove('none');
@@ -28,6 +30,7 @@ cards.forEach(card => {
         dots.classList.add('none');
     })
 
+    // EventListener on readLess click, adding and removing classes with display: none;
     readLess.addEventListener('click', ()=> {
         readMore.classList.remove('none');
         more.classList.add('none');
