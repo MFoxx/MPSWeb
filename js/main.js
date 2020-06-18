e82723236e2a3e287182b6ea68fc9b92ca12378e
MFoxx <mgally@outlook.com>
2020-06-18 12:58:07 +0200

    Scroll Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// SCROLL EFFECT
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { //Vyberie vsetky hrefy ktore maju # s sebe
+    anchor.addEventListener('click', function (e) { //Prida na nich eventlistener
+        e.preventDefault(); //Zabrani defalut(aby sa to na to hned prepolo)
+  
+        //Urobi samotny scroll
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ 
+            behavior: 'smooth'
+        });
+    });
+  });
